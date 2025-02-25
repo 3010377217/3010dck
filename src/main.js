1f86f345ba29ae744b814937f3755fb6c931f037
tarot25223 <3010377217@qq.com>
2025-02-25 10:52:02 +0800

    scss自动导入

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import './assets/main.css'
+
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
@@ -6,6 +6,7 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+import '@/styles/common.scss'
 //测试接口函数
 
 
