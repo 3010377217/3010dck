9f9851819676d3e112cd83b0d217d8720f07d899
tarot25223 <3010377217@qq.com>
2025-02-26 10:50:20 +0800

    home-banner轮播图

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,3 +1,19 @@
+<script setup lang="ts">
+import HomeBanner from './componments/HomeBanner.vue';
+import HomeContegory from './componments/HomeContegory.vue';
+import HomeHot from './componments/HomeHot.vue';
+import HomeNew from './componments/HomeNew.vue';
+import HomeProduct from './componments/HomeProduct.vue';
+
+</script>
+
 <template>
-  <div>home页</div>
+  <div class="container">
+  <HomeContegory/>
+  <HomeBanner/>
+  </div>
+
+  <HomeNew/>
+  <HomeHot/>
+  <HomeProduct/>
 </template>
