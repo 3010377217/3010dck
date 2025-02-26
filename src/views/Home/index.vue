f3245c1b669fd7fde9b5783b14470c9ac8395476
tarot25223 <3010377217@qq.com>
2025-02-26 20:21:59 +0800

    插槽使用-新鲜好物实现

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -3,6 +3,7 @@ import HomeBanner from './componments/HomeBanner.vue';
 import HomeContegory from './componments/HomeContegory.vue';
 import HomeHot from './componments/HomeHot.vue';
 import HomeNew from './componments/HomeNew.vue';
+import HomePanel from './componments/HomePanel.vue';
 import HomeProduct from './componments/HomeProduct.vue';
 
 </script>
@@ -16,4 +17,10 @@ import HomeProduct from './componments/HomeProduct.vue';
   <HomeNew/>
   <HomeHot/>
   <HomeProduct/>
+  <HomePanel title="新鲜好物" subTitle="新鲜好物，好多商品">
+
+  </HomePanel>
+  <HomePanel title="新鲜好物" subTitle="新鲜好物，好多商品">
+
+  </HomePanel>
 </template>
