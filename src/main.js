ea16db89827a2b0e3d187d2fd93c711fe811d20e
tarot25223 <3010377217@qq.com>
2025-02-26 20:56:57 +0800

    定义全局指令-实现图片懒加载

--- src/main.js
+++ src/main.js
@@ -2,11 +2,13 @@
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { useIntersectionObserver } from '@vueuse/core'
 
 import App from './App.vue'
 import router from './router'
 
 import '@/styles/common.scss'
+
 //测试接口函数
 
 
@@ -16,3 +18,19 @@ app.use(createPinia())
 app.use(router)
 
 app.mount('#app')
+//定义全局指令
+app.directive('img-lazy',{
+  mounted(el,binding){
+    //el:指令绑定的元素 img
+    //binding:binding.value 图片url
+    console.log(el,binding.value)
+    useIntersectionObserver(
+      el,
+      ([{ isIntersecting }])=>{
+        console.log(isIntersecting)
+      }
+    )
+  }
+
+
+})
